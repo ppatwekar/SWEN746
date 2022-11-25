fa7f6d2591c5511ab8dc071ab41813cef151cbd6
ppatwekar <99646476+Patwekar@users.noreply.github.com>
2022-11-24 22:37:34 -0500

    Initial project setup

---
+++ sam-api/src/main/java/com/example/sam2023/Sam2023Application.java
@@ -0,0 +1,13 @@
+package com.example.sam2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sam2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sam2023Application.class, args);
+	}
+
+}
---
+++ sam-api/src/test/java/com/example/sam2023/Sam2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sam2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sam2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
