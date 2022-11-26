a47b14e59af50691300c89f9b7aa787d5e52b4cf
ppatwekar <99646476+Patwekar@users.noreply.github.com>
2022-11-26 18:04:03 -0500

    Some daoFiles

---
+++ sam-api/src/main/java/com/example/sam2023/persistance/daoFiles/ReportDAOFile.java
@@ -0,0 +1,8 @@
+package com.example.sam2023.persistance.daoFiles;
+
+import com.example.sam2023.model.Report;
+import com.example.sam2023.persistance.dao.ReportDAO;
+
+public class ReportDAOFile extends AbstractDAOFile<Report> implements ReportDAO{
+    
+}
---
+++ sam-api/src/main/java/com/example/sam2023/persistance/daoFiles/ReviewDAOFile.java
@@ -0,0 +1,8 @@
+package com.example.sam2023.persistance.daoFiles;
+
+import com.example.sam2023.model.Review;
+import com.example.sam2023.persistance.dao.ReviewDAO;
+
+public class ReviewDAOFile extends AbstractDAOFile<Review> implements ReviewDAO {
+    
+}
