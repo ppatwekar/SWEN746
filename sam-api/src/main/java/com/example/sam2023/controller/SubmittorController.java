f3576c7be547ab2fd7c4400d82bc0d3446efa8ee
ppatwekar <99646476+Patwekar@users.noreply.github.com>
2022-11-26 18:23:28 -0500

    Controller Tag fix

--- sam-api/src/main/java/com/example/sam2023/controller/SubmittorController.java
+++ sam-api/src/main/java/com/example/sam2023/controller/SubmittorController.java
@@ -1,7 +1,6 @@
 package com.example.sam2023.controller;
 
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.stereotype.Controller;
 
 @RestController
 public class SubmittorController {
