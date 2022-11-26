434588865f7cdf61814b6cea5d98950a8db65b38
ppatwekar <99646476+Patwekar@users.noreply.github.com>
2022-11-26 18:08:42 -0500

    Submittor dao file

---
+++ sam-api/src/main/java/com/example/sam2023/persistance/daoFiles/AdminDAOFile.java
@@ -0,0 +1,8 @@
+package com.example.sam2023.persistance.daoFiles;
+
+import com.example.sam2023.model.Admin;
+import com.example.sam2023.persistance.dao.AdminDAO;
+
+public class AdminDAOFile extends AbstractDAOFile<Admin> implements AdminDAO{
+    
+}
---
+++ sam-api/src/main/java/com/example/sam2023/persistance/daoFiles/SubmittorDAOFile.java
@@ -0,0 +1,8 @@
+package com.example.sam2023.persistance.daoFiles;
+
+import com.example.sam2023.model.Submittor;
+import com.example.sam2023.persistance.dao.SubmittorDAO;
+
+public class SubmittorDAOFile extends AbstractDAOFile<Submittor> implements SubmittorDAO{
+    
+}
