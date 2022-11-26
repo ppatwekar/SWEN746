43b1fc64452cb748e728d015f71a3554af24ff7f
ppatwekar <99646476+Patwekar@users.noreply.github.com>
2022-11-26 17:40:26 -0500

    PCCService

---
+++ sam-api/src/main/java/com/example/sam2023/service/PCCService.java
@@ -0,0 +1,20 @@
+package com.example.sam2023.service;
+
+import com.example.sam2023.model.Message;
+import com.example.sam2023.model.UserCredential;
+
+public class PCCService implements UserServices{
+
+    @Override
+    public void receiveNotification(int id, Message message) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public boolean authenticateUser(UserCredential u) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+    
+}
