e9440aaaef67e0a8465a42e076b9b4f06baa12f5
ppatwekar <99646476+Patwekar@users.noreply.github.com>
2022-11-29 22:57:31 -0500

    Added method to get directory path

--- sam-api/src/main/java/com/example/sam2023/persistance/filestorageSystem/DirectoryEnum.java
+++ sam-api/src/main/java/com/example/sam2023/persistance/filestorageSystem/DirectoryEnum.java
@@ -3,8 +3,8 @@ package com.example.sam2023.persistance.filestorageSystem;
 public enum DirectoryEnum {
     
     PAPERS("sam-api/src/data/files/papers"),
-    SUBMITTOR_PAPERS("sam-api/src/data/files/papers/submittor_papers"),
-    PCM_PAPERS("sam-api/src/data/files/papers/pcm_papers")
+    SUBMITTOR_PAPERS(PAPERS.getPath()+"/submittor_papers"),
+    PCM_PAPERS(PAPERS.getPath()+"/pcm_papers")
     ;
     private String path;
     private DirectoryEnum(String path){
@@ -13,5 +13,9 @@ public enum DirectoryEnum {
 
     public String getPath(){
         return this.path;
+    }
+
+    public static String makePaperFolderPath(DirectoryEnum en ,int id){
+        return en.getPath()+"/Paper_"+id;
     }
 }
